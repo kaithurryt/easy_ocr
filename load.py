9dcaa3f18bf9969d5da4d13640c5f88fe3091e68
salamer <salamer_gaga@163.com>
2024-12-10 21:19:19 +0800

    first commit

--- load.py
+++ load.py
@@ -12,6 +12,8 @@ easyocr.easyocr.LOGGER.setLevel(logging.DEBUG)
 print("Downloading EasyOCR model...")
 target = "https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/latin.zip"
 
+target2= "https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/craft_mlt_25k.zip"
+
 urlretrieve(target, "model.zip")
 print("Model downloaded successfully. target = ", target)
 
@@ -27,6 +29,13 @@ def download_and_unzip(url, filename, model_storage_directory, verbose=True):
         print("Model unzipped.")
     print("Removing temporary zip file...")
     os.remove(zip_path)
+
+urlretrieve(target2, "model2.zip")
+print("Model downloaded successfully. target = ", target2)
+
+print("Unzipping EasyOCR2 model...")
+download_and_unzip(url=target2, filename="craft_mlt_25k.pth", model_storage_directory="./")
+print("Model unzipped.")
 
 print("Unzipping EasyOCR model...")
 download_and_unzip(url=target, filename="latin.pth", model_storage_directory="./")
