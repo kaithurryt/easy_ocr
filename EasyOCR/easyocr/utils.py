c9f625bca7966af4e7355b4d54bcf89ac27554b0
salamer <salamer_gaga@163.com>
2024-12-10 21:12:23 +0800

    first commit

--- EasyOCR/easyocr/utils.py
+++ EasyOCR/easyocr/utils.py
@@ -623,12 +623,18 @@ def get_image_list(horizontal_list, free_list, img, model_height = 64, sort_outp
     return image_list, max_width
 
 def download_and_unzip(url, filename, model_storage_directory, verbose=True):
+    print("download_and_unzip", url, filename, model_storage_directory, verbose)
     zip_path = os.path.join(model_storage_directory, 'temp.zip')
     reporthook = printProgressBar(prefix='Progress:', suffix='Complete', length=50) if verbose else None
+    print("start download", url, zip_path)
     urlretrieve(url, zip_path, reporthook=reporthook)
+    print("finish download", url, zip_path)
     with ZipFile(zip_path, 'r') as zipObj:
+        print("extract", filename, model_storage_directory)
         zipObj.extract(filename, model_storage_directory)
+    print("finish extract", filename, model_storage_directory)
     os.remove(zip_path)
+    print("remove", zip_path)
 
 def calculate_md5(fname):
     hash_md5 = hashlib.md5()
