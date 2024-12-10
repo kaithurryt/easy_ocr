9a2dd236dd8968330a8849e2485e4d1c2a5d72ae
salamer <salamer_gaga@163.com>
2024-12-10 23:21:28 +0800

    first commit

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import numpy as np
 
 app = flask.Flask(__name__)
 
-
 @app.route("/")
 def index():
     return """
@@ -24,41 +23,31 @@ def index():
     </html>
     """
 
-
 @app.route("/ocr", methods=["POST"])
 def ocr():
     start = time.time()
-    reader = easyocr.Reader(
-        ["ch_sim", "en"],
-        gpu=False,
-        model_storage_directory="./",
-        verbose=False,
-    )
+
+    # 加载 OCR 模型
+    reader = easyocr.Reader(["ch_sim", "en"], gpu=False, model_storage_directory="./", verbose=False)
     print("OCR model loaded, time:", time.time() - start)
-    # 检查是否有文件上传
+
+    # 检查文件上传
     if "image" not in flask.request.files:
         return "No file part"
-
     img_file = flask.request.files["image"]
     if img_file.filename == "":
         return "No selected file"
 
-    print("Start OCR")
-    img = Image.open(img_file)
-
-    img = img.convert("RGB")
-
-    # to numpy
+    # 加载图像并转换为 RGB 格式
+    img = Image.open(img_file).convert("RGB")
     img_bytes_io = io.BytesIO()
     img.save(img_bytes_io, format="JPEG")  # 使用 JPEG 格式保存
-    img = img_bytes_io.getvalue()
+    img_bytes = img_bytes_io.getvalue() # 转换为 NumPy 数组以供 OCR 使用
 
-    print("Start OCR 2")
-    results = reader.readtext(img)
+    print("Start OCR")
+    results = reader.readtext(img_bytes)  # 使用 NumPy 数组作为输入
 
-    print("Start OCR 3")
-
-    # 绘制识别框
+    # 绘制检测框并记录结果
     draw = ImageDraw.Draw(img)
     result_texts = []
     for bbox, text, prob in results:
@@ -68,25 +57,22 @@ def ocr():
         draw.rectangle([x_min, y_min, x_max, y_max], outline="red", width=2)
         result_texts.append(f"Text: {text}, Confidence: {prob:.2f}, Position: {bbox}")
 
-        # 将处理后的图片转换为 base64 编码
+    # 将处理后的图片转换为 base64 编码
     buffered = io.BytesIO()
     img.save(buffered, format="PNG")
     img_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-    # 返回结果页面
-    result_html = f"<h1>OCR Result:</h1>"
+    # 生成结果页面
+    result_html = "<h1>OCR Result:</h1>"
     for result_text in result_texts:
         result_html += f"<p>{result_text}</p>"
-    result_html += (
-        f'<img src="data:image/png;base64,{img_base64}" alt="Processed Image"/>'
-    )
+    result_html += f'<img src="data:image/png;base64,{img_base64}" alt="Processed Image"/>'
 
     end = time.time()
-    print("Take {} seconds".format(end - start))
-    result_html += f"<p>Take {end - start:.2f} seconds</p>"
+    print("OCR completed, time taken: {:.2f} seconds".format(end - start))
+    result_html += f"<p>Time taken: {end - start:.2f} seconds</p>"
 
     return result_html
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
